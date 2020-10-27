cf92acdc52687c7c115c6beaa4a47566f11fbc5d
unknown <gracjanludyga@gmail.com>
2020-10-27 20:45:43 +0100

    refactor app to react-app

--- src/pages/MainPage/components/BasketProceedContainer/styles.js
+++ src/pages/MainPage/components/BasketProceedContainer/styles.js
@@ -24,3 +24,54 @@ export const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
+
+export const ShippingContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  border: 1px solid gray;
+  background-color: lightgray;
+`;
+
+export const BasketSumaryContainer = styled.div`
+  border: 1px solid gray;
+  margin-top: 20px;
+`;
+
+export const SumaryTitle = styled.div`
+display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  border: none
+  background-color: lightgray;
+`;
+
+export const BasketContent = styled.div`
+  padding: 15px 13px;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  padding: 14px 10px;
+  justify-content: space-between;
+  border-bottom: 1px solid gray;
+`;
+
+export const DescriptionContainerGrand = styled(DescriptionContainer)`
+  border: none;
+`;
+
+export const SubmitButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 13px 30px;
+`;
+
+export const SubmitButton = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 14px;
+  padding: 7px 15px;
+  width: 100%;
+`;
